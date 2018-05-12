6e1f1be9b24625ca4f06e83e2ca05cc9efb668e3
MatiasCardullo <36178170+MatiasCardullo@users.noreply.github.com>
2018-05-11 23:58:31 -0300

    Add files via upload

---
+++ TP_2_Cascara/funciones.h
@@ -0,0 +1,26 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+typedef struct {
+
+    char nombre[50];
+    int edad;
+    int estado;
+    int dni;
+
+}EPersona;
+
+void agregaPersona(EPersona [],int,int);
+int validaDNI(EPersona [],int,int);
+void borrarPersona(EPersona []);
+void ordenarPersona(EPersona []);
+void mostrarPersona(EPersona []);
+void graficarPersona(EPersona [],int);
+void inicializaEstructura(EPersona [],int);
+int validaNumero(char [],int);
+int getInt(char [],char [],int ,int ,int);
+void getChar(char [],char [],char [],int);
+int obtenerEspacioLibre(EPersona [],int);
+int buscarPorDni(EPersona [], int ,int);
+
+#endif // FUNCIONES_H_INCLUDED
